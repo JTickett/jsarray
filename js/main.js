fa9c24ce7e456d8571d20763859724638e794702
James Tickett <jtickett@gmail.com>
2024-09-13 18:47:02 +0100

    most basic unstyle form! it checks if the email is valid though lol

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // Stuff to do once the DOM is ready.
 $(document).ready(function(){
-
-
+    $('.form-group').on('submit', function(e){
+        e.preventDefault();
+        console.log('Form submitted');
+    });
 });
