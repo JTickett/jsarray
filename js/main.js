aefb5500b78dd807720a7ad2abf324e6e3015d29
James Tickett <jtickett@gmail.com>
2024-09-23 09:20:28 +0100

    cleaned up the old code removing the selection box and everything linked to it.

--- js/main.js
+++ js/main.js
@@ -11,31 +11,10 @@ $(document).ready(function(){
         storeCurrentlySelectedEmail(inputEmail);
         assignImageToEmail(inputEmail);
 
-
-        //  TODO: I should check that the option does not already exist at this point!!!!
-        //  Add the new option to the select list
-        $("#email-list").val(new Option(inputEmail, inputEmail));
-        setEmailList();
-
-
-        getEmailList();
-        $("#email-input").val("");
-        
     });
     $("#random-image-button").on("click", function (e) {
         loadRandomPicsumImage();
     });
-
-    $("#email-list").on("change", function (e) {
-        const chosenEmail = $(this).val();
-        console.log("Email list changed to: " + chosenEmail);
-        storeCurrentlySelectedEmail(chosenEmail);
-        retrieveCurrentlySelectedEmail();
-    });
-
-    //checkForStoredEmails();
-
-
 
 
 });
